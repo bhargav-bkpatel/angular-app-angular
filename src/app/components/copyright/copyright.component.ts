ceccfb389b0fa3ce97f08e094ebec59393c5ce55
Bhargav <33828401+bhargav-bkpatel@users.noreply.github.com>
2023-04-03 16:59:27 +0530

    Update

---
+++ src/app/components/copyright/copyright.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-copyright',
+  templateUrl: 'copyright.component.html',
+  styleUrls: ['copyright.component.css'],
+})
+export class Copyright {
+  rawzbnn: string = ' '
+  rawortk: string = ' '
+  raw1huy: string = ' '
+
+  constructor() {}
+}
